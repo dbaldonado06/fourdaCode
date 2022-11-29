29fbe77348e50a82f97eb08ab251c8fdfc0b2f79
JogieOtic <jotic@gbox.adnu.edu.ph>
2022-11-29 10:56:25 +0800

    created a form

--- APCS_POS_x_IS/PosForm.vb
+++ APCS_POS_x_IS/PosForm.vb
@@ -1,3 +1,17 @@
 ﻿Public Class PosForm
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
 
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles cancelorderButton.Click
+
+    End Sub
+
+    Private Sub newtransactionButton_Click(sender As Object, e As EventArgs) Handles newtransactionButton.Click
+
+    End Sub
+
+    Private Sub PosForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
